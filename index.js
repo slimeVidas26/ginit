204fda63c407f2c38f352f0a0cbac66bb938bf17
SlimeVidas <dahan.isaac@gmail.com>
2021-04-15 23:01:13 +0300

    github-deps

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const clear = require('clear');
 const figlet = require('figlet')
 const files = require('./lib/files')
 const chalk = require('chalk');
-const inquirer = require('./lib/inquirer');
 const { askGithubCredentials } = require('./lib/inquirer');
 
 clear()
 console.log(chalk.red(figlet.textSync(files.getCurrentDirectoryBase() , {horizontalLayout : 'full'})));
 
-//inquirer.askGithubCredentials();
 const run = async ()=>{
     const credentials = await askGithubCredentials()
     console.log(credentials)
