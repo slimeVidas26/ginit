bb7acd2f87448caf07f78f5515a3118bbd91440d
SlimeVidas <dahan.isaac@gmail.com>
2021-04-15 11:07:42 +0300

    figlet

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet')
+const files = require('./lib/files')
+
+
+clear()
+
+console.log(chalk.blue(figlet.textSync(files.getCurrentDirectoryBase() , {horizontalLayout : 'full'})))
