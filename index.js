4e9877dc76c02ebd0cf051ac5e4de7c954420bcc
SlimeVidas <dahan.isaac@gmail.com>
2021-04-15 11:45:18 +0300

    inquirer

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const chalk = require('chalk');
+
 const clear = require('clear');
 const figlet = require('figlet')
 const files = require('./lib/files')
-
+const chalk = require('chalk');
+const inquirer = require('./lib/inquirer');
+const { askGithubCredentials } = require('./lib/inquirer');
 
 clear()
+console.log(chalk.red(figlet.textSync(files.getCurrentDirectoryBase() , {horizontalLayout : 'full'})));
 
-console.log(chalk.blue(figlet.textSync(files.getCurrentDirectoryBase() , {horizontalLayout : 'full'})))
+//inquirer.askGithubCredentials();
+const run = async ()=>{
+    const credentials = await askGithubCredentials()
+    console.log(credentials)
+}
+
+run()
